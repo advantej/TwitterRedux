9ff10b64818f5eb745eb7b79c7849680908d77cd
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-02-02 17:05:11 -0800

    Initial Commit

---
+++ TwitterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwitterDemo
+//
+//  Created by Tejas Lagvankar on 2/2/15.
+//  Copyright (c) 2015 Yahoo!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
