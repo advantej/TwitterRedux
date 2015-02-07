9042686ab45d07f1c27fed9711efc7c0bb664a39
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-02-07 15:11:28 -0800

    Color tweak

--- TwitterDemo/TweetsViewController.m
+++ TwitterDemo/TweetsViewController.m
@@ -39,7 +39,7 @@
 
 + (UINavigationController *) getWrappedTweetsController {
     UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:[[TweetsViewController alloc] init]];
-    nvc.navigationBar.barTintColor = [UIColor colorWithRed:121.0 / 255.0 green:224.0 / 255.0 blue:237.0 / 255.0 alpha:1];
+    nvc.navigationBar.barTintColor = [UIColor colorWithRed:19.0 / 255.0 green:207.0 / 255.0 blue:232.0 / 255.0 alpha:1];
     nvc.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
     return nvc;
 }
